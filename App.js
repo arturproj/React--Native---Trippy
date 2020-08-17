4a4a2871e8878c85fd3c20ecc983bbd34b5aeddc
Artur DevOps <50926041+arturproj@users.noreply.github.com>
2020-08-17 18:37:50 +0200

    first

---
+++ App.js
@@ -0,0 +1,14 @@
+//import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text } from 'galio-framework'
+//import Navbar from './components/core/navbar'
+
+export default function render (){
+      return (
+      <View>
+      <Text p muted>
+              Hi, I'm a Galio component
+      </Text>
+      </View>
+      ) ;
+}
---
+++ components/core/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavBar } from 'galio-framework';
+
+export default function Navbar() {
+  return (
+    <>
+      <NavBar title="Screen Title" />
+    </>
+  );
+}
